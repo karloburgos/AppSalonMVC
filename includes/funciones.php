9ca665fc74ee1f13ed762878aee3cef837458c53
karloburgos <karlogomez13@outlook.es>
2025-01-11 13:01:28 -0600

    Inicio

---
+++ includes/funciones.php
@@ -0,0 +1,14 @@
+<?php
+
+function printArray($variable) : string {
+    echo "<pre>";
+    var_dump($variable);
+    echo "</pre>";
+    exit;
+}
+
+// Escapa / Sanitizar el HTML
+function s($html) : string {
+    $s = htmlspecialchars($html);
+    return $s;
+}
